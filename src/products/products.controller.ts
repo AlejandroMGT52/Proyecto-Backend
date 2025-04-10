e82a2a72fd1deaec43d6e26c1bbeb105b859c223
Alejandro <alejo.m15097@gmail.com>
2025-04-09 21:26:29 -0500

    Nuevas modificaciones

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,37 +1,74 @@
 import { Controller,Get, Param,Post,HttpCode,Body,HttpStatus,Res,Put, Patch, Delete, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { Product } from './interfaces/product.interface';
+
 
 @Controller('products')
 export class ProductsController {
-
   constructor(private readonly productsService: ProductsService) { }
 
-  // …
-    
+     
   @Get()
-  getAllProducts() {
+  getAllProducts(): Product[] {
     return this.productsService.getAll();
   }
 
+  @Get(':id')
+  find(@Param('id') id: number) {
+    return this.productsService.getId(id);
+  }
+
   @Post()
-  @HttpCode(204)
+  @HttpCode(HttpStatus.NO_CONTENT)
   createProduct(
-    @Body('name') name: string,
-    @Body('description') description: string
+    @Body() body,
   ) {
-    this.productsService.insert({
-      id: this.productsService.getAll().length,
-      name,
-      description
-    });
+    this.productsService.insert(body);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number, 
+    @Body() body,
+  ) {
+    return this.productsService.update(id, body);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id') id: number) {
+    this.productsService.delete(id);
   }
 
 
 
-    @Get('inventario')
-    getHelloInProducts():string{ 
-        return 'estamos en productos'
-     }
+
+
+
+
+
+    //@Get('inventario')
+    //getHelloInProducts():string{ 
+      //  return 'estamos en productos'
+     //}
+
+     //@Get()
+     //getAllProducts(): Product[] {
+     //return this.productsService.getAll();
+     //}
+     
+     //@Post()
+  //@HttpCode(204)
+  //createProduct(
+    //@Body('name') name: string,
+    //@Body('description') description: string
+  //) {
+    //this.productsService.insert({
+      //id: this.productsService.getAll().length,
+      //name,
+      //description
+    //});
+  //}
     
 //  @Get(':id')
 //  find(@Param()params){
@@ -64,23 +101,16 @@ export class ProductsController {
    // createProduct(@Body() body) {
    //    return body;
    // }
-
-   
-   //@Post()
-   //@HttpCode(HttpStatus.NO_CONTENT)
-   //createProduct(@Body() body) {
-   //return body;
-   //}
-
-   //@Get('ruta-error-404')
-   //@HttpCode(HttpStatus.NOT_FOUND)
-   //rutaConError404() {
-  //return 'Esto es un error 404!!';
-  //}
-
-
-
-
+//@Post()
+//@HttpCode(HttpStatus.NO_CONTENT)
+//createProduct(@Body() body) {
+  //return body;
+//}
+@Get('ruta-error-404')
+@HttpCode(HttpStatus.NOT_FOUND)
+rutaConError404() {
+  return 'Esto es un error 404!!';
+}
 //decorador get con parametros 
 // @Get(':id')
 // find(@Res() response, @Param('id') id: number) {
@@ -92,25 +122,27 @@ export class ProductsController {
   
 // }
 // decorador put
-@Put(':id')
-update(@Param('id') id: number, @Body() body) {
-  return `Estás haciendo una operación de actualización del recurso ${id} 
-          con ${body.name} y ${body.description}`;
-}
+
+
+//@Put(':id')
+//update(@Param('id') id: number, @Body() body) {
+ // return `Estás haciendo una operación de actualización del recurso ${id} 
+   //       con ${body.name} y ${body.description}`;
+//}
 //decorador patch 
 @Patch(':id')
 partialUpdate(@Param('id') id: number, @Body() body) {
   return `Actualización parcial del ítem ${id}`;
 }
 //decorador delete 
-@Delete(':id')
-@HttpCode(HttpStatus.NO_CONTENT)
-delete(@Param('id') id: number) {
-  return `Hemos borrado el producto ${id}`;
-}
+//@Delete(':id')
+//@HttpCode(HttpStatus.NO_CONTENT)
+//delete(@Param('id') id: number) {
+  //return `Hemos borrado el producto ${id}`;
+//}
 // metodo Query 
 @Get('query')
 rutaQuery(@Query() query) {
-    return query;
+    return query;
 }
 }
